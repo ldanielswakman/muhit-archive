6f264899d91bcc0bcebf5bc81df1bf9ff57b88f5
ldanielswakman <d.swakman@gmail.com>
2019-11-24 13:02:28 +0100

    Update nav (test menu)

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,25 +1,53 @@
 import React from "react"
+import Logo from "./logo"
 
 export default () => 
 	<React.Fragment>
 		<nav>
 	    <div class="row row-nopadding">
         <div class="col-xs-12 u-aligncenter">
+
           <div class="u-floatright u-nowrap">
-            <a href="/login" class="btn btn-quaternary u-mt15 u-mr5">
+            <a href="/login" class="btn btn-quaternary u-mt15 u-mr5" style={{ opacity: 0.2, pointerEvents: 'none' }}>
               <span class="condensed"><i class="ion ion-log-in ion-15x u-floatleft"></i></span>
               <span class="extended">LOG IN</span>
             </a>
-            <a href="/login/facebook" class="btn btn-facebook u-mr5 u-mt15">
+            <a href="/login/facebook" class="btn btn-facebook u-mr5 u-mt15" style={{ opacity: 0.2, pointerEvents: 'none' }}>
               <i class="ion ion-social-facebook ion-15x u-floatleft u-ph5"></i>
               <span class="extended">
                 <span class="u-floatleft u-mt5">CONNECT</span>
               </span>
             </a>
             <span class="extended">
-              <a href="/register" class="btn btn-primary u-mt15 u-mr15">SIGN UP</a>
+              <a href="/register" class="btn btn-primary u-mt15 u-mr15" style={{ opacity: 0.2, pointerEvents: 'none' }}>SIGN UP</a>
             </span>
           </div>
+            
+          <a href="/" id="nav_logo" className="u-floatleft">
+              <Logo />
+          </a>
+
+          <ul class="menu">
+              <li className="active">
+                  <a href="/" className="u-nowrap">
+                      <i class="ion ion-android-home ion-15x"></i>
+                      Story
+                  </a>
+              </li>
+              <li>
+                  <a href="/report" className="u-nowrap">
+                      <i class="ion ion-clipboard ion-15x"></i>
+                      Report
+                  </a>
+              </li>
+              <li>
+                  <a href="/archive" className="u-nowrap">
+                      <i class="ion ion-lightbulb ion-15x"></i>
+                      Idea archive
+                  </a>
+              </li>
+          </ul>
+
         </div>
       </div>
   	</nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,12 @@ import Layout from "../components/layout"
 
 export default () => (
 	<Layout>
+
 		<Nav />
-		Hello world!
+
+		<div style={{ textAlign: 'center', marginTop: '5rem' }}>
+			<h1>Muhit Story, Research report & Idea archive</h1>
+		</div>
+		
 	</Layout>
 )
